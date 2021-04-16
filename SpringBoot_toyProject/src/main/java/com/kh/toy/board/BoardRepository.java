4c98058e864f0ced465cca3586b43935cb7c0154
azimemory <killsky2014@naver.com>
2021-04-16 21:43:25 +0900

    수업자료 첨부

--- SpringBoot_toyProject/src/main/java/com/kh/toy/board/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kh.toy.board;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardRepository  extends JpaRepository<Board, Long> {
-	public Board findBoardByBdIdxAndUserId(Long bdIdx, String userId);
-}
